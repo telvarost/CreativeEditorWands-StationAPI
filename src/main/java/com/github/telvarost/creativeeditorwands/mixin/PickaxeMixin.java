f5061420e85cf92c47525860f7f9013303be9fee
telontelvarost <telontelvarost@gmail.com>
2024-04-21 17:51:15 -0400

    UDOC-0 - Enable block id picking using pickaxe

--- src/main/java/com/github/telvarost/creativeeditorwands/mixin/PickaxeMixin.java
+++ src/main/java/com/github/telvarost/creativeeditorwands/mixin/PickaxeMixin.java
@@ -1,18 +1,51 @@
 package com.github.telvarost.creativeeditorwands.mixin;
 
+import com.github.telvarost.creativeeditorwands.ModHelper;
 import net.minecraft.block.BlockBase;
+import net.minecraft.entity.player.PlayerBase;
+import net.minecraft.item.ItemBase;
+import net.minecraft.item.ItemInstance;
 import net.minecraft.item.tool.Pickaxe;
 import net.minecraft.item.tool.ToolBase;
 import net.minecraft.item.tool.ToolMaterial;
+import net.minecraft.level.Level;
+import net.modificationstation.stationapi.api.client.item.CustomTooltipProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Pickaxe.class)
-public class PickaxeMixin extends ToolBase {
+public class PickaxeMixin extends ToolBase implements CustomTooltipProvider {
     /** - Change paint/erase color with wooden pickaxe */
     @Shadow
     private static BlockBase[] effectiveBlocks;
     public PickaxeMixin(int i, ToolMaterial arg) {
         super(i, 1, arg, effectiveBlocks);
+    }
+
+    @Override
+    public String[] getTooltip(ItemInstance itemInstance, String originalTooltip) {
+        if (  (this.id == ItemBase.woodPickaxe.id)
+           && (ModHelper.ModHelperFields.enableWorldEditTools)
+        ) {
+            return new String[]{"Color Picker", "Color: " + itemInstance.getDamage()};
+        } else {
+            return new String[]{originalTooltip};
+        }
+    }
+
+    @Override
+    public boolean useOnTile(ItemInstance item, PlayerBase player, Level level, int i, int j, int k, int meta) {
+        if (  (this.id == ItemBase.woodPickaxe.id)
+           && (ModHelper.ModHelperFields.enableWorldEditTools)
+        ) {
+            int blockId = level.getTileId(i, j, k);
+            int blockMeta = level.getTileMeta(i, j, k);
+
+            item.setDamage(blockId);
+
+            return true;
+        } else {
+            return false;
+        }
     }
 }
