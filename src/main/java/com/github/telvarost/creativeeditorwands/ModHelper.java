7f0240cd9c455a7bb9af5b0dc16eff0e0ea019a8
telontelvarost <telontelvarost@gmail.com>
2024-04-21 16:02:20 -0400

    UDOC-0 - Remove damage dealt to wooden swords

--- src/main/java/com/github/telvarost/creativeeditorwands/ModHelper.java
+++ src/main/java/com/github/telvarost/creativeeditorwands/ModHelper.java
@@ -3,6 +3,7 @@ package com.github.telvarost.creativeeditorwands;
 public class ModHelper {
 
     public static class ModHelperFields {
+        public static Boolean enableWorldEditTools = true;
         public static Integer PICK_BLOCK_COLOR = 0;
     }
 }
--- src/main/java/com/github/telvarost/creativeeditorwands/mixin/SwordMixin.java
+++ src/main/java/com/github/telvarost/creativeeditorwands/mixin/SwordMixin.java
@@ -1,15 +1,55 @@
 package com.github.telvarost.creativeeditorwands.mixin;
 
+import com.github.telvarost.creativeeditorwands.ModHelper;
+import net.minecraft.entity.Living;
+import net.minecraft.entity.player.PlayerBase;
 import net.minecraft.item.ItemBase;
+import net.minecraft.item.ItemInstance;
 import net.minecraft.item.tool.Sword;
 import net.minecraft.item.tool.ToolMaterial;
+import net.minecraft.level.Level;
 import net.modificationstation.stationapi.api.item.tool.StationSwordItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Sword.class)
 public class SwordMixin extends ItemBase implements StationSwordItem {
-    /** - Paint with wooden sword */
+    /**
+     * - Paint with wooden sword
+     */
     public SwordMixin(int i, ToolMaterial arg) {
         super(i);
+    }
+
+    @Inject(at = @At("HEAD"), method = "postHit", cancellable = true)
+    public void creativeEditorWands_postHit(ItemInstance arg, Living arg2, Living arg3, CallbackInfoReturnable<Boolean> cir) {
+        if (  (this.id == ItemBase.woodSword.id)
+           && (ModHelper.ModHelperFields.enableWorldEditTools)
+        ) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "postMine", cancellable = true)
+    public void creativeEditorWands_postMine(ItemInstance arg, int i, int j, int k, int l, Living arg2, CallbackInfoReturnable<Boolean> cir) {
+        if (  (this.id == ItemBase.woodSword.id)
+           && (ModHelper.ModHelperFields.enableWorldEditTools)
+        ) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Override
+    public boolean useOnTile(ItemInstance item, PlayerBase player, Level level, int i, int j, int k, int meta) {
+        if (  (this.id == ItemBase.woodSword.id)
+           && (ModHelper.ModHelperFields.enableWorldEditTools)
+        ) {
+
+            return true;
+        } else {
+            return false;
+        }
     }
 }
