15be7d98c3bccda5cffafc6cf88409ec330ee363
telontelvarost <telontelvarost@gmail.com>
2024-04-22 23:52:43 -0400

    UDOC-0 - Fix iteration mistake when selection is one block wide; Add visual for selection

--- src/main/java/com/github/telvarost/creativeeditorwands/mixin/AxeMixin.java
+++ src/main/java/com/github/telvarost/creativeeditorwands/mixin/AxeMixin.java
@@ -89,33 +89,53 @@ public class AxeMixin extends ToolBase implements CustomTooltipProvider {
                     int add_x = 0;
                     int add_y = 0;
                     int add_z = 0;
+                    boolean allowSingleIteration_X = false;
+                    boolean allowSingleIteration_Y = false;
+                    boolean allowSingleIteration_Z = false;
 
                     if (ModHelper.ModHelperFields.copyPoint1_X < ModHelper.ModHelperFields.copyPoint2_X) {
                         add_x = 1;
                     } else if (ModHelper.ModHelperFields.copyPoint1_X > ModHelper.ModHelperFields.copyPoint2_X) {
                         add_x = -1;
+                    } else {
+                        allowSingleIteration_X = true;
                     }
 
                     if (ModHelper.ModHelperFields.copyPoint1_Y < ModHelper.ModHelperFields.copyPoint2_Y) {
                         add_y = 1;
                     } else if (ModHelper.ModHelperFields.copyPoint1_Y > ModHelper.ModHelperFields.copyPoint2_Y) {
                         add_y = -1;
+                    } else {
+                        allowSingleIteration_Y = true;
                     }
 
                     if (ModHelper.ModHelperFields.copyPoint1_Z < ModHelper.ModHelperFields.copyPoint2_Z) {
                         add_z = 1;
                     } else if (ModHelper.ModHelperFields.copyPoint1_Z > ModHelper.ModHelperFields.copyPoint2_Z) {
                         add_z = -1;
+                    } else {
+                        allowSingleIteration_Z = true;
                     }
 
-                    for(int var6 = ModHelper.ModHelperFields.copyPoint1_X; var6 != (ModHelper.ModHelperFields.copyPoint2_X + add_x); var6 += add_x) {
-                        for(int var7 = ModHelper.ModHelperFields.copyPoint1_Y; var7 != (ModHelper.ModHelperFields.copyPoint2_Y + add_y); var7 += add_y) {
-                            for(int var8 = ModHelper.ModHelperFields.copyPoint1_Z; var8 != (ModHelper.ModHelperFields.copyPoint2_Z + add_z); var8 += add_z) {
+                    boolean stateIteration_X = allowSingleIteration_X;
+                    boolean stateIteration_Y = allowSingleIteration_Y;
+                    boolean stateIteration_Z = allowSingleIteration_Z;
+
+                    for(int var6 = ModHelper.ModHelperFields.copyPoint1_X; (stateIteration_X || var6 != (ModHelper.ModHelperFields.copyPoint2_X + add_x)); var6 += add_x) {
+                        for(int var7 = ModHelper.ModHelperFields.copyPoint1_Y; (stateIteration_Y || var7 != (ModHelper.ModHelperFields.copyPoint2_Y + add_y)); var7 += add_y) {
+                            for(int var8 = ModHelper.ModHelperFields.copyPoint1_Z; (stateIteration_Z || var8 != (ModHelper.ModHelperFields.copyPoint2_Z + add_z)); var8 += add_z) {
                                 level.setTile(var6, var7, var8, blockId);
                                 level.setTileMeta(var6, var7, var8, blockMeta);
+
+                                stateIteration_Z = false;
                             }
+                            stateIteration_Z = allowSingleIteration_Z;
+                            stateIteration_Y = false;
                         }
+                        stateIteration_Y = allowSingleIteration_Y;
+                        stateIteration_X = false;
                     }
+                    stateIteration_X = allowSingleIteration_X;
                 }
             } else if (2 == item.count) {
                 /** - Paste selection */
@@ -129,32 +149,44 @@ public class AxeMixin extends ToolBase implements CustomTooltipProvider {
                     int add_x = 0;
                     int add_y = 0;
                     int add_z = 0;
+                    boolean allowSingleIteration_X = false;
+                    boolean allowSingleIteration_Y = false;
+                    boolean allowSingleIteration_Z = false;
 
                     if (ModHelper.ModHelperFields.copyPoint1_X < ModHelper.ModHelperFields.copyPoint2_X) {
                         add_x = 1;
                     } else if (ModHelper.ModHelperFields.copyPoint1_X > ModHelper.ModHelperFields.copyPoint2_X) {
                         add_x = -1;
+                    } else {
+                        allowSingleIteration_X = true;
                     }
 
                     if (ModHelper.ModHelperFields.copyPoint1_Y < ModHelper.ModHelperFields.copyPoint2_Y) {
                         add_y = 1;
                     } else if (ModHelper.ModHelperFields.copyPoint1_Y > ModHelper.ModHelperFields.copyPoint2_Y) {
                         add_y = -1;
+                    } else {
+                        allowSingleIteration_Y = true;
                     }
 
                     if (ModHelper.ModHelperFields.copyPoint1_Z < ModHelper.ModHelperFields.copyPoint2_Z) {
                         add_z = 1;
                     } else if (ModHelper.ModHelperFields.copyPoint1_Z > ModHelper.ModHelperFields.copyPoint2_Z) {
                         add_z = -1;
+                    } else {
+                        allowSingleIteration_Z = true;
                     }
 
                     int offset_x = 0;
                     int offset_y = 0;
                     int offset_z = 0;
+                    boolean stateIteration_X = allowSingleIteration_X;
+                    boolean stateIteration_Y = allowSingleIteration_Y;
+                    boolean stateIteration_Z = allowSingleIteration_Z;
 
-                    for(int var6 = ModHelper.ModHelperFields.copyPoint1_X; var6 != (ModHelper.ModHelperFields.copyPoint2_X + add_x); var6 += add_x) {
-                        for(int var7 = ModHelper.ModHelperFields.copyPoint1_Y; var7 != (ModHelper.ModHelperFields.copyPoint2_Y + add_y); var7 += add_y) {
-                            for(int var8 = ModHelper.ModHelperFields.copyPoint1_Z; var8 != (ModHelper.ModHelperFields.copyPoint2_Z + add_z); var8 += add_z) {
+                    for(int var6 = ModHelper.ModHelperFields.copyPoint1_X; (stateIteration_X || var6 != (ModHelper.ModHelperFields.copyPoint2_X + add_x)); var6 += add_x) {
+                        for(int var7 = ModHelper.ModHelperFields.copyPoint1_Y; (stateIteration_Y || var7 != (ModHelper.ModHelperFields.copyPoint2_Y + add_y)); var7 += add_y) {
+                            for(int var8 = ModHelper.ModHelperFields.copyPoint1_Z; (stateIteration_Z || var8 != (ModHelper.ModHelperFields.copyPoint2_Z + add_z)); var8 += add_z) {
 
                                 int copyBlockId = level.getTileId(var6, var7, var8);
                                 int copyBlockMeta = level.getTileMeta(var6, var7, var8);
@@ -162,14 +194,20 @@ public class AxeMixin extends ToolBase implements CustomTooltipProvider {
                                 level.setTileMeta(i + offset_x, j + offset_y, k + offset_z, copyBlockMeta);
 
                                 offset_z += add_z;
+                                stateIteration_Z = false;
                             }
+                            stateIteration_Z = allowSingleIteration_Z;
                             offset_z = 0;
                             offset_y += add_y;
+                            stateIteration_Y = false;
                         }
+                        stateIteration_Y = allowSingleIteration_Y;
                         offset_y = 0;
                         offset_x += add_x;
+                        stateIteration_X = false;
                     }
                     offset_x = 0;
+                    stateIteration_X = allowSingleIteration_X;
                 }
             } else {
                 /** - Select/clear points */
@@ -183,6 +221,7 @@ public class AxeMixin extends ToolBase implements CustomTooltipProvider {
                     ModHelper.ModHelperFields.copyPoint2_X = Integer.MAX_VALUE;
                     ModHelper.ModHelperFields.copyPoint2_Y = Integer.MAX_VALUE;
                     ModHelper.ModHelperFields.copyPoint2_Z = Integer.MAX_VALUE;
+                    item.setDamage(0);
                 } else if (  (Integer.MAX_VALUE != ModHelper.ModHelperFields.copyPoint1_X)
                           && (Integer.MAX_VALUE != ModHelper.ModHelperFields.copyPoint1_Y)
                           && (Integer.MAX_VALUE != ModHelper.ModHelperFields.copyPoint1_Z)
@@ -190,10 +229,12 @@ public class AxeMixin extends ToolBase implements CustomTooltipProvider {
                     ModHelper.ModHelperFields.copyPoint2_X = i;
                     ModHelper.ModHelperFields.copyPoint2_Y = j;
                     ModHelper.ModHelperFields.copyPoint2_Z = k;
+                    item.setDamage(1);
                 } else {
                     ModHelper.ModHelperFields.copyPoint1_X = i;
                     ModHelper.ModHelperFields.copyPoint1_Y = j;
                     ModHelper.ModHelperFields.copyPoint1_Z = k;
+                    item.setDamage(item.getDurability() / 2);
                 }
             }
 
